3a0dbc1cfd299fdde7e41915e5c1a3a3b8ddab1a
Noufal Ahmed <42862497+nou-96@users.noreply.github.com>
2019-02-07 15:21:47 +0530

    Update leapyr.py

--- leapyr.py
+++ leapyr.py
@@ -1,5 +1,5 @@
-year=int(input())
-if((year%400==0) or ((year%4==0) and (year%100!=0))):
+years=int(input())
+if((years%400==0) or ((years%4==0) and (years%100!=0))):
   print("yes")
 else:
   print("no")
