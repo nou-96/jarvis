a209256aa2facd37c065b7883e4c434073f77dd6
Noufal Ahmed <42862497+nou-96@users.noreply.github.com>
2019-02-08 10:09:28 +0530

    Update primeornot.py

--- primeornot.py
+++ primeornot.py
@@ -1,7 +1,7 @@
-num=int(input(""))
+numb=int(input(""))
 k=0
-for i in range(2,num//2+1):
-    if(num%i==0):
+for i in range(2,numb//2+1):
+    if(numb%i==0):
         k=k+1
 if(k<=0):
     print("yes")
